0b15d90f945d12696b903411c5aa06d7d68da4ae
efeanilarslan <142424465+efeanilarslan@users.noreply.github.com>
2025-10-17 15:07:18 +0300

    Update Cap.py

--- Cap.py
+++ Cap.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#pip install numpy matplotlib
 
 f = float(input("Sinyalin Frekansi ne olmali (Hz) "))
 Genlik_max = float(input("Max genlik ne olmali "))
@@ -10,53 +11,86 @@ t = np.linspace(0, k*T, 1000)
 
 v_anlk = Genlik_max * np.sin(2 * np.pi * f * t + Faz)
 
-# │ ┤ ╢ ║ ┐ └ ┴ ├ ─ ┼ ╞ ═ ╬ ╧ ╤ ╨ ┘ ┌ █ 
-
-print("Devre1 Yüksek Geçiren Filtre")
-print(" (+) ————C——┐——— (+)  ")
-print("            │         ")
-print("   Vin      R    Vout ")
-print("            │         ")
-print("     ———————└——— (-)  ")
-print(" ")
-print("Devre2 Alçak Geçiren Filtre")
-print(" (+) ————R——┐——— (+)  ")
-print("            │         ")
-print("   Vin      C    Vout ")
-print("            │         ")
-print("     ———————└——— (-)  ")
-x = int(input("Hangi devre kullanilacak?(1,2)"))
 r=float(input("Direnc değeri(Ohm)"))
 c=float(input("Kapasitor değeri(Farat)"))
+
 Xc=1 / (2 * np.pi * f * c)
 Z = np.sqrt(r**2 + Xc**2)
 
-if x == 1:
-    Genlik_max_cikis = Genlik_max * (r / Z)
-    Cfaz = np.arctan(Xc / r)
-elif x == 2:
-    Genlik_max_cikis = Genlik_max * (Xc / Z)
-    Cfaz = -np.arctan(r / Xc)
-else:
-    print("Geçersiz seçim!")
-    Genlik_max_cikis = 0
-    Cfaz = 0
+Genlik_max_cikis_yüksekgeç = Genlik_max * (r / Z)
+Cfaz_yüksekgeç = np.arctan(Xc / r)
+Genlik_max_cikis_alçakgeç = Genlik_max * (Xc / Z)
+Cfaz_alçakgeç = -np.arctan(r / Xc)
 
-v_cikis = Genlik_max_cikis * np.sin(2 * np.pi * f * t + Faz + Cfaz)
+v_cikis_yüksekgeç = Genlik_max_cikis_yüksekgeç * np.sin(2 * np.pi * f * t + Faz + Cfaz_yüksekgeç)
+v_cikis_alçakgeç = Genlik_max_cikis_alçakgeç * np.sin(2 * np.pi * f * t + Faz + Cfaz_alçakgeç)
 
-fig, axs = plt.subplots(2, 1, figsize=(15,10))
+layout ="""
+cc
+ab
+de
+"""
 
-axs[0].plot(t, v_anlk)
-axs[0].set_title("Sinüs dalgasi "+ str(f) +"Hz")
-axs[0].set_xlabel("Zaman (saniye)")
-axs[0].set_ylabel("Genlik")
-axs[0].grid(True)
+fig, axs = plt.subplot_mosaic(layout, figsize=(12, 8))
 
-axs[1].plot(t, v_cikis)
-axs[1].set_title("V çikis")
-axs[1].set_xlabel("Zaman (saniye)")
-axs[1].set_ylabel("Genlik")
-axs[1].grid(True)
+axs["a"].plot()
+axs["a"].set_xticks([])
+axs["a"].set_yticks([])
+axs["a"].plot()
+axs["a"].set_xlim(0,8)
+axs["a"].set_ylim(0,8)
+axs["a"].plot([3,4],[6,6])
+axs["a"].plot([4,4],[5.6,6.4])
+axs["a"].plot([4.05,4.05],[5.6,6.4])
+axs["a"].plot([4.05,5.5],[6,6])
+axs["a"].plot([4.5,4.5],[6,4.5])
+axs["a"].plot([4.5,4.6,4.4,4.6,4.4,4.6,4.4,4.5],[4.5,4.4,4.3,4.2,4.1,4,3.9,3.8])
+axs["a"].plot([4.5,4.5],[3.8,2.3])
+axs["a"].plot([3,5.5],[2.3,2.3])
+axs["a"].set_title("Yüksek Geçiren Filitre")
+axs["a"].text(3,3.5, "Vin\n" + str(f)+"Hz\n" + "Vmax:" + str(Genlik_max))
+axs["a"].text(5.4,5, "Vout")
+axs["a"].text(3.8,6.8, str(c)+"Farat")
+axs["a"].text(4.65,4, str(r)+"Ohm")
+
+axs["b"].plot()
+axs["b"].set_xticks([])
+axs["b"].set_yticks([])
+axs["b"].set_xlim(0,8)
+axs["b"].set_ylim(0,8)
+axs["b"].plot([3,4],[6,6])
+axs["b"].plot([4,4.03,4.06,4.09,4.12,4.15,4.18,4.21],[6,6.3,5.7,6.3,5.7,6.3,5.7,6])
+axs["b"].plot([4.21,5.5],[6,6])
+axs["b"].plot([4.5,4.5],[6,4.5])
+axs["b"].plot([4.4,4.6],[4.5,4.5])
+axs["b"].plot([4.4,4.6],[4.3,4.3])
+axs["b"].plot([4.5,4.5],[4.3,2.3])
+axs["b"].plot([3,5.5],[2.3,2.3])
+axs["b"].set_title("Alçak Geçiren Filitre")
+axs["b"].text(3,3.5, "Vin\n" + str(f)+"Hz\n" + "Vmax:" + str(Genlik_max) )
+axs["b"].text(5.4,5, "Vout")
+axs["b"].text(4.65,4, str(c)+" Farat")
+axs["b"].text(3.8,6.8, str(r)+" Ohm")
+
+axs["c"].plot(t, v_anlk)
+axs["c"].set_title("Sinüs dalgasi "+ str(f) +"Hz")
+axs["c"].set_xlabel("Zaman (saniye)")
+axs["c"].set_ylabel("Genlik")
+axs["c"].grid(True)
+
+axs["d"].plot(t, v_cikis_yüksekgeç)
+axs["d"].set_title("Yüksek Geçiren Çikis")
+axs["d"].set_xlabel("Zaman (saniye)")
+axs["d"].set_ylabel("Genlik")
+axs["d"].plot([0,k*1/f],[Genlik_max_cikis_yüksekgeç,Genlik_max_cikis_yüksekgeç])
+axs["d"].grid(True)
+
+axs["e"].plot(t, v_cikis_alçakgeç)
+axs["e"].set_title("Alçak Geçiren Çikis")
+axs["e"].set_xlabel("Zaman (saniye)")
+axs["e"].set_ylabel("Genlik")
+axs["e"].plot([0,k*1/f],[Genlik_max_cikis_alçakgeç,Genlik_max_cikis_alçakgeç])
+axs["e"].grid(True)
 
 plt.tight_layout()
 plt.show()
